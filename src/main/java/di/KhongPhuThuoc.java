98e40c3a79a327bd0a179cea3e2227160f9930e8
DELL <phanphuocho14i1@gmail.com>
2017-06-01 19:24:42 +0700

    DI inject theo cach xml cach 1.2(Ham dung constructor)

--- src/main/java/di/KhongPhuThuoc.java
+++ src/main/java/di/KhongPhuThuoc.java
@@ -6,11 +6,13 @@ package di;
 public class KhongPhuThuoc {
     private PhuThuoc1 phuThuoc1;
 
-    public int add(int a,int b){
+    public KhongPhuThuoc(PhuThuoc1 phuThuoc1) {
+        this.phuThuoc1 = phuThuoc1;
+    }
+
+    public int add(int a, int b){
         return phuThuoc1.add(a,b);
     }
 
-    public void setPhuThuoc1(PhuThuoc1 phuThuoc1) {
-        this.phuThuoc1 = phuThuoc1;
-    }
+
 }
